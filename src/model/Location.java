d6ff5902e1c9eb5b7f1cc4594d91f1ab8f4e5c6f
Matthew Lemons <lemonsmatt13@gmail.com>
2018-04-13 14:17:58 -0400

    added constructors and fields

--- src/model/Location.java
+++ src/model/Location.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Location {
+    private String name;
+    private int id;
+    private float latitude;
+    private float longitude;
+    private float rating;
+    private float radius;
+
+    public Location(String name, float latitude, float longitude, float radius) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radius = radius;
+    }
+
+    public Location(String name, int id, float latitude, float longitude, float rating, float radius) {
+        this.name = name;
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.rating = rating;
+        this.radius = radius;
+    }
+
+
 }
