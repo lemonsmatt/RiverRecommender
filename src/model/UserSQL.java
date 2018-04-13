4fe27e2609482e9cddaa71b370f0a156edf0abdd
Eric1e <24831984+Eric1e@users.noreply.github.com>
2018-04-13 15:59:27 -0400

    Add files via upload
    BannedBy needed adminUserName.

---
+++ src/model/UserSQL.java
@@ -0,0 +1,60 @@
+package model;
+
+public class UserTester implements UserInterface {
+
+    @Override
+    public boolean isAdmin(String email, String password) {
+		String query = "SELECT IsAdmin FROM dbo.[User] WHERE Email = " + email + " Password = " + password + ";";  
+		statement.executeUpdate(query);
+		
+		boolean ret = false;
+		try {
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				ret = rs.getString("IsAdmin");
+			}
+			
+			return ret == "True" ? true : false;
+		}
+        return false;
+    }
+
+    @Override
+    public boolean isValidUser(String email, String password) {
+		String query = "SELECT BannedBy FROM dbo.[User] WHERE Email = " + email + " Password = " + password + ";";  
+		statement.executeUpdate(query);
+		
+		boolean ret = false;
+		try {
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				ret = rs.getString("BannedBy");
+			}
+			
+			return ret == null ? true : false;
+		}
+        return false;
+    }
+
+    @Override
+    public boolean banUser(String email, String AdminUserName) {
+		//Do we need to know admin who banned the user? (BannedBy) 
+		
+		String query = "UPDATE dbo.[User] SET BannedBy = " + AdminUserName + " WHERE Email = " + email = ";";  
+		statement.executeUpdate(query);
+		
+		boolean ret = false;
+		try {
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				ret = rs.getString("BannedBy");
+			}
+			
+			return ret != null ? true : false;
+		}
+        return false;
+    }
+}
