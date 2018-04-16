c2f00bce981498527d1806bf9926a6567f5d0b82
Matthew Lemons <lemonsmatt13@gmail.com>
2018-04-16 18:33:26 -0400

    added test data

--- src/model/LocationTester.java
+++ src/model/LocationTester.java
@@ -1,12 +1,25 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationTester implements LocationInterface {
 
-    @Override
+    public static List<Location> locList = new ArrayList<Location>();
+
+    public LocationTester() {
+        if (locList.isEmpty())
+        {
+            locList.add(new Location("Utah", 1,32, 71, 3,2, "lemons"));
+            locList.add(new Location("Zion", 2,35, 61, 1,9, "lemons"));
+            locList.add(new Location("Springdale", 3,32, 11, 3,3, "lemons"));
+            locList.add(new Location("Kolob", 4,12, 81, 2,5, "lemons"));
+        }
+    }
+
+        @Override
     public List<Location> getValidatedLocations() {
-        return null;
+        return locList;
     }
 
     @Override
@@ -16,11 +29,14 @@ public class LocationTester implements LocationInterface {
 
     @Override
     public boolean addLocation(Location loc) {
+        if (loc.getName() !=null){
+            return  true;
+        }
         return false;
     }
 
     @Override
     public boolean removeLocation(Location loc) {
-        return false;
+        return locList.remove(loc);
     }
 }
